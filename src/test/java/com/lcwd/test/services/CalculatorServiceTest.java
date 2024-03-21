6697e3c6238f8f4ba75f446469ebc3bed5f281a3
ashish-yubi <ashish.mohanka@go-yubi.com>
2024-03-21 15:54:32 +0530

    JUNIT 5

--- src/test/java/com/lcwd/test/services/CalculatorServiceTest.java
+++ src/test/java/com/lcwd/test/services/CalculatorServiceTest.java
@@ -1,24 +1,26 @@
 package com.lcwd.test.services;
 
-import org.junit.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorServiceTest
 {
     //Test Method of add Two Numbers
 
-    @BeforeClass //This help in initializing  logic before executing any test  cases
-    public static void init()
-    {
-        System.out.println("Before all Test case I am Executed :) ");
-        System.out.println("Testing Begin");
-    }
-
-    @Before
-    public void beforeEach()
-    {
-        //This Executes before each test case
-        System.out.println("I Execute Before Each Test Case");
-    }
+//    @BeforeClass //This help in initializing  logic before executing any test  cases
+//    public static void init()
+//    {
+//        System.out.println("Before all Test case I am Executed :) ");
+//        System.out.println("Testing Begin");
+//    }
+//
+//    @Before
+//    public void beforeEach()
+//    {
+//        //This Executes before each test case
+//        System.out.println("I Execute Before Each Test Case");
+//    }
 
     @Test
     public void addTwoNumbersTest()
@@ -26,31 +28,31 @@ public class CalculatorServiceTest
         System.out.println("Test Beginning for addTwoNumbers");
         int result=CalculatorService.addTwoNumbers(12,45);
         int expected=57;
-        Assert.assertEquals(expected,result);
+        Assertions.assertEquals(expected,result);
         System.out.println("Test Ending for addTwoNumbers");
     }
 
-    @Test(timeout = 2000)
+    @Test
     public void sumAnyNumbersTest() throws InterruptedException {
         //It has time limit of 2 seconds
         System.out.println("Test Beginning for addAnyNumbers");
         int result=CalculatorService.sumAnyNumbers(2,8,9,1,9);
         int expected=29;
         //Thread.sleep(3000);
-        Assert.assertEquals(expected,result);
+        Assertions.assertEquals(expected,result,"Test Failed");
         System.out.println("Test Ending for addAnyNumbers");
     }
 
-    @After
-    public void afterEach()
-    {
-        System.out.println("I Execute After Each Test Case");
-    }
-
-    @AfterClass //This Executes after all test cases
-    public static void cleanup()
-    {
-        System.out.println("Testing Ended");
-        System.out.println("After all Test case I am Executed :) ");
-    }
+//    @After
+//    public void afterEach()
+//    {
+//        System.out.println("I Execute After Each Test Case");
+//    }
+//
+//    @AfterClass //This Executes after all test cases
+//    public static void cleanup()
+//    {
+//        System.out.println("Testing Ended");
+//        System.out.println("After all Test case I am Executed :) ");
+//    }
 }
